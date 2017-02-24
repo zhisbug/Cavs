5b0b7ce21aa1cc642f60db5db644c19f1139f407
xushizhen <xuszh2008@163.com>
2017-02-24 11:32:42 -0500

    a sample of deep learning model, not compiled yet

---
+++ apps/lenet-5/lenet-5.cc
@@ -0,0 +1,17 @@
+#include "cavs/frontend/cxx/sym.h"
+#include "cavs/frontend/cxx/session.h"
+
+int main() {
+  Sym kernel1 = Sym::Variable(C_FLOAT, {1, 5, 5, 20});
+  Sym kernel2 = Sym::Variable(C_FLOAT, {20, 5, 5, 50});
+  Sym fc1 = Sym::Variable(C_FLOAT, {500, 800});
+  Sym fc2 = Sym::Variable(C_FLOAT, {10, 500});
+
+  Sym input = Sym::MnistInput(100, "/users/shizhenx/projects/Cavs/apps/lenet-5/data");
+  Sym loss = input.Conv(kernel1).Maxpooling(2, 2).Conv(kernel2).Maxpooling(2, 2).
+              FullyConnected(fc1).Relu().FullyConnected(fc2).SoftmaxEntropyLogits();
+  Sym train = loss.Optimizer();
+
+  Session sess;
+  sess.Run({train});
+}
