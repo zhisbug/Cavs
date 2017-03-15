ee943ae3b1199b8592d016f1c3cfca51f2e8d6d5
xushizhen <xuszh2008@163.com>
2017-03-15 17:56:12 -0400

    more test on odd-even sort

--- cavs/backend/functor_sort_scan.cuh
+++ cavs/backend/functor_sort_scan.cuh
@@ -16,10 +16,9 @@ __device__ inline void Comparator(T& valA, T& valB, bool direction) {
 template<typename T, unsigned SHARE_SIZE_LIMIT>
 __global__ void BatchedMergeSort(T *inout, unsigned int batch, unsigned int N, bool direction) {
   __shared__ T s_val[SHARE_SIZE_LIMIT];
-
-  T* d_val = inout + blockIdx.x*SHARE_SIZE_LIMIT + threadIdx.x;
+  T* d_val = inout + blockIdx.x*N+ threadIdx.x;
   s_val[threadIdx.x] = d_val[0];
-  s_val[threadIdx.x+(SHARE_SIZE_LIMIT)/2] = d_val[SHARE_SIZE_LIMIT/2];
+  s_val[threadIdx.x+N/2] = d_val[N/2];
 
   for (unsigned size = 2; size <= N; size <<= 1) {
     unsigned stride = size / 2; 
@@ -40,7 +39,7 @@ __global__ void BatchedMergeSort(T *inout, unsigned int batch, unsigned int N, b
   }
   __syncthreads();
   d_val[0] = s_val[threadIdx.x];
-  d_val[SHARE_SIZE_LIMIT/2] = s_val[threadIdx.x+(SHARE_SIZE_LIMIT)/2];
+  d_val[N/2] = s_val[threadIdx.x+(N)/2];
 }
 
 } //namespace backend
--- cavs/backend/functor_sort_scan_test.cu
+++ cavs/backend/functor_sort_scan_test.cu
@@ -1,5 +1,6 @@
 #include "cavs/backend/functor_sort_scan.cuh"
 #include "cavs/util/logging.h"
+#include "cavs/util/macros_gpu.h"
 
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
@@ -11,23 +12,41 @@
 using namespace backend;
 /*using namespace std;*/
 
+const int SHARE_SIZE_LIMIT = 1 << 13;
 
 int main() {
-  //10k documents
-  thrust::host_vector<int> h_vec(1<<3);
-  thrust::generate(h_vec.begin(), h_vec.end(), rand);
-  for (int i = 0; i < h_vec.size(); i++)
-    LOG(INFO) << "h_vec[" << i << "]: " << h_vec[i];
-  thrust::device_vector<int> d_vec = h_vec;
-  /*thrust::sort(d_vec.begin(), d_vec.end());*/
-  int threadsPerBlock = 1<<3;
-  int blocksPerGrid = 1;
-  bool direction = true;//small first
-  BatchedMergeSort< int, 1<<3 ><<<blocksPerGrid, threadsPerBlock>>>(
-      thrust::raw_pointer_cast(d_vec.data()),
-      1, 1<<3, direction);
-  thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
-  LOG(INFO);
-  for (int i = 0; i < h_vec.size(); i++)
-    LOG(INFO) << "h_vec[" << i << "]: " << h_vec[i];
+  //8k documents
+  for (int Batch = 1; Batch <= 1 << 13; Batch <<= 1) {
+    for (int N = 1; N <= 1 << 10; N <<= 1) {
+      thrust::host_vector<int> h_vec(Batch*N);
+      thrust::generate(h_vec.begin(), h_vec.end(), rand);
+      thrust::device_vector<int> d_vec = h_vec;
+      thrust::host_vector<int> h_vec_verity = h_vec;
+      thrust::device_vector<int> d_vec_verify = h_vec;
+      LOG(INFO) << "Testing with N = " << N
+                << "\tand Batch = " << Batch << "\t...";
+      int threadsPerBlock = (N+1)/2;
+      int blocksPerGrid = Batch;
+      bool direction = true;//small first
+      CHECK(N <= SHARE_SIZE_LIMIT);
+      BatchedMergeSort<int, SHARE_SIZE_LIMIT><<<blocksPerGrid, threadsPerBlock>>>(
+          thrust::raw_pointer_cast(d_vec.data()),
+          Batch, N, direction);
+      checkCudaError(cudaGetLastError());
+      thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+      for (int i = 0; i < Batch; i++)
+        thrust::sort(d_vec_verify.begin()+i*N, d_vec_verify.begin()+(i+1)*N);
+      thrust::copy(d_vec_verify.begin(), d_vec_verify.end(), h_vec_verity.begin());
+      for (int i = 0; i < Batch; i++) {
+        for (int j = 0; j < N; j++) {
+          CHECK((h_vec[i*N+j] == h_vec_verity[i*N+j]) == direction)
+               << "h_vec[" << i << "][" << j << "]: "
+               << h_vec[i*N+j]
+               << "\th_vec_verify[" << i << "][" << j << "]: "
+               << h_vec_verity[i*N+j];
+        }
+      }
+      LOG(INFO) << "Test Passed!";
+    }
+  }
 }
