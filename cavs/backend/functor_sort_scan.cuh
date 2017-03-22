49a8dd4091a0051c2d7dfc4e562b83f7a1f355d2
xushizhen <xuszh2008@163.com>
2017-03-22 16:53:00 -0400

    1st version of 2-level batched odd-even sort for large scale vocabulary

--- cavs/backend/functor_sort_scan.cuh
+++ cavs/backend/functor_sort_scan.cuh
@@ -17,9 +17,13 @@ __device__ inline void Comparator(T& valA, T& valB, bool direction) {
   }
 }
 
-template <typename T, unsigned int SHARE_SIZE_LIMIT>
-__global__ void BatchedMergeSort(T* out, const T* in, unsigned int N, bool direction) {
-  __shared__ T s_val[SHARE_SIZE_LIMIT];
+//SHARE_SIZE_LIMIT should be equal to 2*blockDim.x
+//In this case, N is less or equal to SHARE_SIZE_LIMIT
+/*template <typename T, unsigned int SHARE_SIZE_LIMIT>*/
+template <typename T>
+__global__ void BatchedOddEvenSortInCache(T* out, const T* in,
+    unsigned int N, bool direction) {
+  extern __shared__ T s_val[];
   const T* in_val = in + blockIdx.x*N+ threadIdx.x;
   T* out_val = out + blockIdx.x*N+ threadIdx.x;
   s_val[threadIdx.x] = in_val[0];
@@ -37,7 +41,6 @@ __global__ void BatchedMergeSort(T* out, const T* in, unsigned int N, bool direc
       if (pos+stride < N) {
         Comparator(s_val[pos], s_val[pos+stride], direction);
       }
-      /*printf("%d\t%d\t%d\n", threadIdx.x, s_val[pos], s_val[pos+stride]);*/
       stride >>= 1;
     }
     for (; stride > 0; stride >>= 1) {
@@ -51,10 +54,67 @@ __global__ void BatchedMergeSort(T* out, const T* in, unsigned int N, bool direc
   }
   __syncthreads();
   out_val[0] = s_val[threadIdx.x];
-  /*if (threadIdx.x + (N+1)/2 < N)*/
-    /*d_val[(N+1)/2] = s_val[threadIdx.x+(N+1)/2];*/
   if (threadIdx.x + blockDim.x < N)
     out_val[blockDim.x] = s_val[threadIdx.x+blockDim.x];
+}
+
+//Assume each strip has been sorted with BatchedOddEvenSortInCache
+//This function STRIDED sort this blocks
+//Instead of merge sort, we still use odd-even sort,
+//but all the data are in the global memory now
+//The thread configuration is exactly the same with the aforementioned function
+template <typename T>
+__global__ void BatchedOddEvenSortStride(T* inout,
+    unsigned int N, bool direction) {
+  const SORTED_BLOCK = 2*blockDim.x;
+
+  //SORTED_BLOCK is 2x larger than any threadIdx.x
+  for (unsigned int outer_stride = SORTED_BLOCK; outer_stride < N;
+       outer_stride <<= 1) {
+    for (unsigned int strip = 0;
+         strip < (N+2*outer_stride-1) / (2*outer_stride); strip++) {
+      unsigned int offset_of_strip = strip*2*outer_stride;
+      for (unsigned int stride = outer_stride; stride > 0; stride >>= 1) {
+        if (stride < (outer_stride / 2)) {
+          for (unsigned int round = 0; round < outer_stride/blockDim.x; round++) {
+            //We don't have enough threads here,
+            //so each thread has to do more and
+            //pretend to behave as the virtual thread id(s);
+            unsigned int virtual_id = threadIdx.x + round*blockDim.x;
+            unsigned int offset_within_stride = virtual_id & (stride-1);
+            unsigned int pos = 2*virtual_id - offset_within_stride + offset_of_strip;
+            if (offset_within_stride > stride && pos < N) {
+              Comparator(inout[pos-stride], inout[pos], direction);
+            }
+            __syncthreads();
+          }
+        }else {
+          for (unsigned int round = 0; round < outer_stride/blockDim.x; round++) {
+            unsigned int virtual_id = threadIdx.x + round*blockDim.x;
+            unsigned int offset_within_stride = virtual_id & (stride-1);
+            if (pos+stride < N) {
+              Comparator(inout[pos], inout[pos+stride], direction);
+            }
+            __syncthreads();
+          }
+        }
+      }
+    }
+  }
+}
+
+template <typename T>
+void BatchedOddEvenSort(T* out, const T* in, unsigned int N, unsigned int Batch) {
+  const int MAX_THREADS_IN_BLOCK = 1 << 10;
+  unsigned int threadsPerBlock =
+      (MAX_THREADS_IN_BLOCK > N)? N : MAX_THREADS_IN_BLOCK;
+  unsigned int blocksPerGrid = Batch;
+  /*LOG(INFO) << blocksPerGrid << "\t" << threadsPerBlock;*/
+
+  BatchedOddEvenSortInCache<T><<<blocksPerGrid, threadsPerBlock,
+                       threadsPerBlock*sizeof(T)>>>(out, in, N);
+  BatchedOddEvenSortStride<T><<<blocksPerGrid, threadsPerBlock>>>(out, N);
+  checkCudaError(cudaGetLastError());
 }
 
 //N == blockDim.x
@@ -104,8 +164,6 @@ __global__ void BatchedScanInCache(T* out, const T* in, unsigned int N) {
 
 //Assume each stride has been scanned with BatchedScanInCache
 //This function STRIDED scans this blocks
-//We control the blockDim.x so that the in[id] and out[id]
-//will not exceed the upper bound of the arrays.
 template <typename T>
 __global__ void BatchedScanStride(T* out, const T* in, unsigned int N) {
   __shared__ T pre_scan;
@@ -130,7 +188,7 @@ void BatchedScan(T* out, const T* in, unsigned int N, unsigned int Batch) {
   unsigned int threadsPerBlock =
       (MAX_THREADS_IN_BLOCK > N)? N : MAX_THREADS_IN_BLOCK;
   unsigned int blocksPerGrid = Batch;
-  LOG(INFO) << blocksPerGrid << "\t" << threadsPerBlock;
+  /*LOG(INFO) << blocksPerGrid << "\t" << threadsPerBlock;*/
 
   BatchedScanInCache<T><<<blocksPerGrid, threadsPerBlock,
                        threadsPerBlock*sizeof(T)>>>(out, in, N);
