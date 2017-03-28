b4bfe22d1a76e07c7bbefd01f1a724b4f8165c3d
xushizhen <xuszh2008@163.com>
2017-03-27 19:25:52 -0600

    adding data/ddv operator decl

--- cavs/backend/op_decl_placeholder.cc
+++ cavs/backend/op_decl_placeholder.cc
@@ -6,7 +6,7 @@ namespace backend {
 
 class PlaceholderOpDecl : public OpDecl {
  public:
-  PlaceholderOpDecl(const OpDef& def) : OpDecl(def) {};
+  PlaceholderOpDecl(const OpDef& def) : OpDecl(def) {}
   //void MakeGradient(vector<OpDef>* grad) override {}
   void ShapeInference(vector<TensorShapeDef>* out_shape,
     const vector<TensorShapeDef>& inputs) override;
@@ -20,6 +20,31 @@ void PlaceholderOpDecl::ShapeInference(
   out_shape->at(0) = op_def_.shape(0);
 }
 
+class DataOpDecl : public OpDecl {
+ public:
+  DataOpDecl(const OpDef& def) : OpDecl(def) {}
+  void ShapeInference(vector<TensorShapeDef>* out_shape,
+    const vector<TensorShapeDef>& inputs) override;
+};
+
+void DataOpDecl::ShapeInference(
+    vector<TensorShapeDef>* out_shape,
+    const vector<TensorShapeDef>& inputs) {
+  CHECK(out_shape);
+  CHECK(out_shape->empty());
+  CHECK(op_def_.shape_size() == 0);
+  int batch = GetSingleArg<int>(op_def_, "Batch");
+  const std::vector<int>& shape = GetListArg<int>(op_def_, "Shape");
+  CHECK(!shape.empty());
+  CHECK(shape.size() > 1);
+  out_shape->resize(1);
+  out_shape->at(0).add_dim(batch);
+  for (int i = 1; i < shape.size(); i++) {
+    out_shape->at(0).add_dim(shape[i]);
+  }
+}
+
 REGISTER_OP_DECL_BUILDER("Placeholder", PlaceholderOpDecl);
+REGISTER_OP_DECL_BUILDER("Data", DataOpDecl);
 
 } //namespace backend
--- cavs/backend/op_decl_variable.cc
+++ cavs/backend/op_decl_variable.cc
@@ -20,6 +20,32 @@ void VariableOpDecl::ShapeInference(
   out_shape->at(0) = op_def_.shape(0);
 }
 
+class DDVOpDecl : public OpDecl{
+ public:
+  DDVOpDecl(const OpDef& def) : OpDecl(def) {};
+  //void MakeGradient(vector<OpDef>* grad) override {}
+  void ShapeInference(vector<TensorShapeDef>* out_shape,
+    const vector<TensorShapeDef>& inputs) override;
+};
+
+void DDVOpDecl::ShapeInference(
+    vector<TensorShapeDef>* out_shape,
+    const vector<TensorShapeDef>& inputs) {
+  CHECK(out_shape);
+  CHECK(out_shape->empty());
+  CHECK(op_def_.shape_size() == 0);
+  int batch = GetSingleArg<int>(op_def_, "Batch");
+  const std::vector<int>& shape = GetListArg<int>(op_def_, "Shape");
+  CHECK(!shape.empty());
+  CHECK(shape.size() > 1);
+  out_shape->resize(1);
+  out_shape->at(0).add_dim(batch);
+  for (int i = 1; i < shape.size(); i++) {
+    out_shape->at(0).add_dim(shape[i]);
+  }
+}
+
 REGISTER_OP_DECL_BUILDER("Variable", VariableOpDecl);
+REGISTER_OP_DECL_BUILDER("DDV", DDVOpDecl);
 
 } //namespace backend
