8e14118a871312809e99801d9f2039bf31ced713
xushizhen <xuszh2008@163.com>
2017-08-25 02:20:30 -0400

    update the batching for weight update policy

--- cavs/midend/batch_weight_updater.h
+++ cavs/midend/batch_weight_updater.h
@@ -14,28 +14,48 @@ class BatchingWeightUpdater {
     CHECK(finalize_node->empty());
     
     Scope* s = nodes->front()->scope();
-    std::list<Node*> updater;
     std::set<Node*> blacklist;
     for (auto iter = nodes->begin(); iter != nodes->end(); ) {
       CHECK((*iter)->scope() == s);
-      bool cond0 = ((*iter)->output_size() == 1);
-      bool cond1 = (*iter)->output(0)->isGradient();
-      if (cond0 && cond1 && blacklist.find(*iter) == blacklist.end()) {
-        Edge* backward_e = (*iter)->output(0);
-        Edge* forward_e = s->FindEdge(GetOriginName(backward_e->name()));  
-        CHECK(forward_e);
-        bool cond2 = !forward_e->IsBatchEnabled();
-        if (cond2) {
-          updater.push_back(*iter);
-          finalize_node->push_back(*iter);
-          nodes->erase(iter++);
-          continue;
+      bool isSingleOutput = ((*iter)->output_size() == 1);
+      bool isBackwardAndNotInGatherPullPath = false;
+      for (Edge* e : (*iter)->output()) {
+        if (e->isGradient()) {
+          Edge* forward_e     = s->FindEdge(GetOriginName(e->name()));  
+          CHECK(forward_e);
+          if (!forward_e->IsBatchEnabled()) {
+            isBackwardAndNotInGatherPullPath = true; 
+            break;
+          }
+        } 
+      }
+
+      VLOG(V_DEBUG) << (*iter)->debug_info();
+      VLOG(V_DEBUG) << isSingleOutput << "\t" << isBackwardAndNotInGatherPullPath;
+
+      if (blacklist.find(*iter) != blacklist.end()) {
+        for (Edge* edge : (*iter)->output()) {
+          if (edge->scope() == (*iter)->scope()) {
+            for (Node* pnode : edge->dst(true)) {
+              blacklist.insert(pnode);
+            }
+          }
         }
-      }else if ((!cond0 && cond1) || blacklist.find(*iter) != blacklist.end()) {
-        blacklist.insert(*iter);
-        for (Edge* e : (*iter)->output()) {
-          for (Node* pn : e->dst(true)) {
-            blacklist.insert(pn);
+      }else {
+        if (isBackwardAndNotInGatherPullPath) {
+          if (isSingleOutput) {
+            finalize_node->push_back(*iter);
+            nodes->erase(iter++);
+            continue;
+          }else {
+            blacklist.insert(*iter);
+            for (Edge* edge : (*iter)->output()) {
+              if (edge->scope() == (*iter)->scope()) {
+                for (Node* pnode : edge->dst(true)) {
+                  blacklist.insert(pnode);
+                }
+              }
+            }
           }
         }
       }
@@ -43,6 +63,7 @@ class BatchingWeightUpdater {
     }
 
     for (Node* n : *finalize_node) {
+      CHECK(n->output_size() == 1);
       bool cond0 = (n->output(0)->scope() == n->scope());
       if (cond0) {
         for (Node* pn : n->output(0)->dst(true)) {
@@ -51,6 +72,15 @@ class BatchingWeightUpdater {
         }
       }
     }
+
+    for (auto *n : *finalize_node) {
+      VLOG(V_DEBUG) << n->debug_info();
+    }
+    CHECK(!blacklist.empty());
+    for (auto *n : blacklist) {
+      VLOG(V_DEBUG) << n->debug_info();
+    }
+    //LOG(FATAL) << "here";
   }
 };
 
