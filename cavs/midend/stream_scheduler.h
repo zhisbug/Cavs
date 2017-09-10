b3a7de1e8051f0c14136b1c7212edd900ceacf19
xushizhen <xuszh2008@163.com>
2017-09-10 19:34:24 -0400

    many-stream version, but the event record overhead is too high

--- cavs/midend/stream_scheduler.h
+++ cavs/midend/stream_scheduler.h
@@ -23,6 +23,7 @@ class StreamScheduler {
     std::set<int> using_stream_ids;
     std::set<int> used_stream_ids;
     std::set<Edge*> inherited_stream;
+    //null stream will not be recorded
     std::unordered_map<Edge*, int> edge_stream_record;
     std::unordered_map<Edge*, int> edge_to_last_src_nodeid;
     std::unordered_map<Edge*, int> edge_dealed_times;
@@ -31,44 +32,50 @@ class StreamScheduler {
     auto iter = nodes.begin();
     for (int id = 0; id < nodes.size(); id++, iter++) {
       CHECK(stmts->at(id)->type() == Statement::EXPR);
+      int stream_id = -1;
       if ((*iter)->name() != "Gather" &&
           (*iter)->name() != "Scatter") {
         //bool assigned = false;
-        int stream_id = -1;
+        CHECK(stmts->at(id)->type() == Statement::EXPR);
+        ExprStatement* es = dynamic_cast<ExprStatement*>(stmts->at(id));
         for (Edge* edge : (*iter)->input()) {
           if (edge->scope() == (*iter)->scope()) {
             if (edge_dealed_times.find(edge) == edge_dealed_times.end())
               edge_dealed_times[edge] = 0;
-            if (++edge_dealed_times[edge] == edge->dst_size(true)) {
-              CHECK(edge_stream_record.find(edge) != edge_stream_record.end());
-              using_stream_ids.erase(edge_stream_record[edge]);
-              used_stream_ids.insert(edge_stream_record[edge]);
-            }
 
-            ExprStatement* es = dynamic_cast<ExprStatement*>(stmts->at(id));
             if (stream_id == -1) {
               //inherit logic
-              if (inherited_stream.find(edge) == inherited_stream.end()) {
-                CHECK(edge_stream_record.find(edge) != edge_stream_record.end());
+              if (inherited_stream.find(edge) == inherited_stream.end() &&
+                  edge_stream_record.find(edge) != edge_stream_record.end()) {
                 stream_id = edge_stream_record[edge];
-                es->GetContext()->SetStreamId(stream_id);
                 inherited_stream.insert(edge);
-              }else {//fork logic
+                VLOG(V_DEBUG) << (*iter)->name() << ": Inheriting stream[ " << stream_id << "]";
+              }
+              
+              if (stream_id == -1){//fork logic
                 //get an id first
                 if (!used_stream_ids.empty()) {
                   stream_id = *used_stream_ids.begin();
-                  CHECK(using_stream_ids.find(stream_id) == using_stream_ids.end());
+                  CHECK(using_stream_ids.find(stream_id) == using_stream_ids.end())
+                    << stream_id;
                   used_stream_ids.erase(stream_id);
+                  VLOG(V_DEBUG) << (*iter)->name() << ": Swapping stream[ "
+                                << stream_id << "] in used";
                 }else {
                   stream_id = StreamEventHandlePool::GenNewStreamID();
+                  CHECK(used_stream_ids.find(stream_id) == used_stream_ids.end());
                 }
               }
+              CHECK(stream_id != -1);
               using_stream_ids.insert(stream_id);
+              VLOG(V_DEBUG) << (*iter)->name() << ": Inserting stream[ "
+                            << stream_id << "] in using";
               es->GetContext()->SetStreamId(stream_id);
             }
 
-            CHECK(edge_stream_record.find(edge) != edge_stream_record.end());
-            if (stream_id != edge_stream_record[edge]) {
+            CHECK(stream_id != -1); 
+            if (edge_stream_record.find(edge) != edge_stream_record.end() &&
+                stream_id != edge_stream_record[edge]) {
               //sync then
               CHECK(edge_to_last_src_nodeid.find(edge) != edge_to_last_src_nodeid.end());
               int last_node_id = edge_to_last_src_nodeid[edge];
@@ -83,10 +90,34 @@ class StreamScheduler {
               prev_stmt->GetContext()->SetEventRecord(event_id);
               es->GetContext()->SetWaitForEventId(event_id);
             }
+
+            if (edge_dealed_times.find(edge) != edge_dealed_times.end() &&
+                ++edge_dealed_times[edge] == edge->dst_size(true) &&
+                edge_stream_record.find(edge) != edge_stream_record.end() &&
+                stream_id != edge_stream_record[edge]) {
+              VLOG(V_DEBUG) << (*iter)->name() << ": Erasing stream["
+                            << edge_stream_record[edge]
+                            << "] in using and swapping it to used";
+              using_stream_ids.erase(edge_stream_record[edge]);
+              used_stream_ids.insert(edge_stream_record[edge]);
+            }
           }
         }
 
-        CHECK(stream_id > 0);
+        if (stream_id == -1) {
+          if (!used_stream_ids.empty()) {
+            stream_id = *used_stream_ids.begin();
+            CHECK(using_stream_ids.find(stream_id) == using_stream_ids.end()) 
+              << stream_id;
+            used_stream_ids.erase(stream_id);
+          }else {
+            stream_id = StreamEventHandlePool::GenNewStreamID();
+          }
+          CHECK(stream_id != -1);
+          using_stream_ids.insert(stream_id);
+          es->GetContext()->SetStreamId(stream_id);
+        }
+
         for (Edge* edge : (*iter)->output()) {
           if (edge->scope() == (*iter)->scope()) {
             if (edge_stream_record.find(edge) == edge_stream_record.end()) {
@@ -99,7 +130,11 @@ class StreamScheduler {
           }
         }
       }
+
+      LOG(INFO) << "Streaming policy: " << (*iter)->name()
+                << " ==> Stream[" << stream_id << "]";
     }
+    //LOG(FATAL) << "here";
 
     //CHECK(stmts->at(node_id_prev)->type() == Statement::EXPR);
     //ExprStatement* record_es = dynamic_cast<ExprStatement*>(stmts->at(node_id_prev));
